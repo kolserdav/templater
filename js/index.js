76cac6e2bafc97651412d980509f22bb0e83753b
kolserdav <serega12101983@gmail.com>
2018-04-15 23:23:52 +0800

    save23

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 const   ajax = require('ajaxsim'),
         cook = require('dist-cookie');
 
-let jsonFile = document.getElementsByTagName('script')[0].src;
+let jsonFile = (document.getElementsByTagName('script'))[0].src;
 let body = document.querySelector('body');
 
 
 
-if (!navigator.onLine){
+
     //window.location = str;
-    ajax(jsonFile, '', call);
-}
+
 if(navigator.onLine) {
     let nameCookie = 'name';
     let cookieData = cook(nameCookie),
@@ -17,18 +16,23 @@ if(navigator.onLine) {
         host = window.location,
         response = "cookie=" + cookieData + '&host=' + host + '&title=' + titlePage;
     ajax('http://templater.col/respond-require-data', response);
+    ajax(jsonFile, '', parseJson);
 }
 
 
-function call(data){
+if(!navigator.onLine){
+    let loc = 'http://templater.col/cache/users/' + window.localStorage.nameLineCi + '/offline.html';
+    if (window.location !== loc) {
+        window.location = loc;
+    }
+}
+
+
+
+function parseJson(data, i = 0){
+
     let obj = JSON.parse(data);
-    let dir = obj.info.name;
-    window.location = 'http://templater.col/cache/users/'+dir+'/offline.html';
-    parseJson(obj);
-}
-
-function parseJson(obj, i = 0){
-
+    window.localStorage.nameLineCi = obj.info.name;
     if (i < obj.pages.count) {
         let page = obj.pages['page-' + i];
         for (key in page) {
